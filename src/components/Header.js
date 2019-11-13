33d942c9b78e854c0649006fa0d209807f59a800
mihir13 <45789327+mihir13@users.noreply.github.com>
2019-11-13 18:25:40 +0000

    background

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ export default class Header extends Component {
          <div className="row banner">
             <div className="banner-text">
                <h1 className="responsive-headline">{resumeData.name}</h1>
-               <h3 style={{color:'#F7F4E9', fontFamily:'sans-serif '}}>I am a {resumeData.role}.</h3>
+               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.</h3>
                <hr/>
                <ul className="social">
                   {
